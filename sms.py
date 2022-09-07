5b0603253328f4eda7b975b46bc1a115d46252df
haikal059 <43695662+haikal059@users.noreply.github.com>
2022-09-07 14:34:48 +0700

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,53 +1,42 @@
 from time import sleep
-import requests
 from bs4 import BeautifulSoup
+import requests
 
+while True:
+    sleep(1)
+    cookies =  {
+    '_uc_referrer': 'https://www.bing.com/',
+    '_pbjs_userid_consent_data': '3524755945110770',
+    'ts': '1662534792',
+    }
 
-cookies = {
-    '_ga': 'GA1.2.2021142155.1659227818',
-    '__gads': 'ID=2921c594f2561082-22dd80e697d400b3:T=1659227819:RT=1659227819:S=ALNI_MbVkkkYiET2sW7DtzWUmo0UFQR9fg',
-    '__gpi': 'UID=00000822b8942ee6:T=1659227819:RT=1659227819:S=ALNI_MaDAoqMlaYbrO9IlQvJ9XlK169GCQ',
-    '_ga_LLL30FE8DG': 'GS1.1.1659227818.1.1.1659227924.0',
-    'PHPSESSID': 'utseppt2qm6osgpq2nhdf79rvg',
-    'phps': '1662430121',
-}
-
-
-headers = {
-    'authority': 'receive-smss.com',
+    headers = {
+    'authority': 'receivesms.cc',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
-    'cache-control': 'max-age=0',
     # Requests sorts cookies= alphabetically
-    # 'cookie': '_ga=GA1.2.2021142155.1659227818; __gads=ID=2921c594f2561082-22dd80e697d400b3:T=1659227819:RT=1659227819:S=ALNI_MbVkkkYiET2sW7DtzWUmo0UFQR9fg; __gpi=UID=00000822b8942ee6:T=1659227819:RT=1659227819:S=ALNI_MaDAoqMlaYbrO9IlQvJ9XlK169GCQ; _ga_LLL30FE8DG=GS1.1.1659227818.1.1.1659227924.0; PHPSESSID=utseppt2qm6osgpq2nhdf79rvg; phps=1662430121',
-    'referer': 'https://receive-smss.com/sms/15714788202/',
+    # 'cookie': '_uc_referrer=https://www.bing.com/; _pbjs_userid_consent_data=3524755945110770; ts=1662534792',
+    'referer': 'https://receivesms.cc/sms/13478188349',
     'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
 }
 
+    url = 'https://receivesms.cc/sms/13478188349'
 
+    req = requests.post(url, cookies=cookies, headers=headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
 
-while True:
-    sleep(2)
-    url = 'https://receive-smss.com/sms/14438394437/'
-    res = requests.get(url, cookies=cookies, headers=headers)
-    
-    soup = BeautifulSoup(res.content, 'html.parser')
-    
-    
+    html = soup.findAll('ul')[3:4]
 
-    sms = soup.findAll('tr')[2:3]
+    for txt in html:
+       print(txt.text)
 
-    for el in sms:
-        print(el.text)
-        
         
 
     
